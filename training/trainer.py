654a6c16d8b7f4212c9dcca5e97b7eb78851856f
Pawel Gozdur <pawel.gozdur@sportvision.tech>
2023-01-14 16:06:15 +0100

    trainer abstract methods

--- training/trainer.py
+++ training/trainer.py
@@ -1,20 +1,35 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any
-
 import torch
+from torch.utils.data import IterableDataset
+from torch.optim import Optimizer
+
+
 class Trainer(ABC):
     def __init__(self, config):
-        self.oprtimizer = create_optimizer()
+        self.optimizer: Optimizer = self.create_optimizer()
+        self.dataloader: IterableDataset = self.create_dataloader()
+        self.model = self.load_model()
 
     @abstractmethod
     def train(self):
         pass
+
     @abstractmethod
-    def create_optimizer(self, optimizer: Dict[str, Any]):
+    def create_optimizer(self) -> Optimizer:
         pass
 
     @abstractmethod
     def create_dataloader(self):
+        pass
+
+    @abstractmethod
+    def load_model(self):
+        pass
+
+    @abstractmethod
+    def save_model(self):
+        pass
 
 
 if __name__ == "__main__":
