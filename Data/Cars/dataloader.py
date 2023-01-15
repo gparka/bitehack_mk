e09655dccf49b3593fe534bd42b3f96e82550f51
Pawel Gozdur <pawel.gozdur@sportvision.tech>
2023-01-15 01:14:44 +0100

    paths

--- Data/Cars/dataloader.py
+++ Data/Cars/dataloader.py
@@ -7,8 +7,6 @@ class CarsData:
     def get_dataloader(self, download=True, save=True):
         if download:
             path = "http://co2cars.apps.eea.europa.eu/tools/download?download_query=http%3A%2F%2Fco2cars.apps.eea.europa.eu%2F%3Fsource%3D%7B%22track_total_hits%22%3Atrue%2C%22query%22%3A%7B%22bool%22%3A%7B%22must%22%3A%5B%7B%22constant_score%22%3A%7B%22filter%22%3A%7B%22bool%22%3A%7B%22must%22%3A%5B%7B%22bool%22%3A%7B%22should%22%3A%5B%7B%22term%22%3A%7B%22year%22%3A2021%7D%7D%5D%7D%7D%2C%7B%22bool%22%3A%7B%22should%22%3A%5B%7B%22term%22%3A%7B%22scStatus%22%3A%22Provisional%22%7D%7D%5D%7D%7D%5D%7D%7D%7D%7D%5D%7D%7D%2C%22display_type%22%3A%22tabular%22%7D&download_format=csv"
-        else:
-            path = '.\..\Datasets\Cars.csv'
         self.df = pd.read_csv(path).drop(
             ['ID', 'VFN', 'Mp', 'Man',
                 'MMS', 'T', 'Va', 'Ve', 'Cn', 'Ct', 'Cr', 'At1 (mm)', 'At2 (mm)',
@@ -20,7 +18,7 @@ class CarsData:
         for col_name in self.keys:
             if self.df[col_name].dtype == 'object':
                 if col_name == 'Mk':
-                    with open('.\\resources\car_manufacturrers.yml') as f:
+                    with open('/home/sportv09/PycharmProjects/bitehack_mk/resources/cleanData.csv') as f:
                         data = yaml.load(f, Loader=SafeLoader)
                         data = [x.upper() for x in data]
                         n = len(col_vals)
@@ -45,6 +43,10 @@ class CarsData:
             self.row_list.append(row.to_list())
 
         if save:
-            self.df.to_csv('.\\resources\cleanData.csv')
+            self.df.to_csv('/home/sportv09/PycharmProjects/bitehack_mk/resources/cleanData.csv')
 
         return self.row_list
+
+if __name__ == "__main__":
+    cars = CarsData()
+    cars.get_dataloader()
