0f6694ab75c2f671a0c2aa2c96ee0d6814549111
piotr310100 <56043614+piotr310100@users.noreply.github.com>
2023-01-15 06:36:24 +0100

    New data:

--- Data/Cars/dataloader.py
+++ Data/Cars/dataloader.py
@@ -2,9 +2,11 @@ import pandas as pd
 import yaml
 from yaml.loader import SafeLoader
 import torch
-from torchvision import transforms
+from torchvision import transforms, datasets
 import numpy as   np
 from torch.utils.data import Dataset, DataLoader
+from os import walk
+from PIL import Image
 
 class CarsData:
   def get_data(self, download = False, save = False):
@@ -69,6 +71,68 @@ class CarDataset(Dataset):
 def get_dataloader(data, batch_s, shuff = True, n_workers = 0):
   return DataLoader(data, batch_size = batch_s, shuffle = shuff, num_workers = n_workers)
 
+class ImgDataset(Dataset):
+    def __init__(self, df):
+      self.df = df
+      self.keys = self.df.keys()
+      self.norm_reverse = [0]
+      for col_name in self.keys:
+        if col_name != 'Img':
+          m = self.df[col_name].max()
+          if m != 0:
+            self.df[col_name] = self.df[col_name] / m 
+          self.norm_reverse.append(m)
+    
+    def __len__(self):
+      return len(self.df)
+    
+    def __getitem__(self, idx):
+      if torch.is_tensor(idx):
+        idx = idx.tolist()
+      img = self.df.iloc[idx, 0]
+      #img = transforms.ToTensor()(img)
+      data = self.df.iloc[idx, 1:]
+      data = np.asarray(data)
+      data = torch.Tensor(data)
+      return (img, data)
+
+def get_images():
+  keys = ['Make', 'Model', 'Year', 'MSRP', 'Front Wheel Size (in)', 'SAE Net Horsepower @ RPM',
+'Displacement', 'Engine Type', 'Width, Max w/o mirrors (in)', 'Height, Overall (in)',
+'Length, Overall (in)', 'Gas Mileage', 'Drivetrain', 'Passenger Capacity', 'Passenger Doors',
+'Body Style']
+  filenames = walk(".\\resources\Car_img")
+  for dirpath, _, names in filenames:
+    img_params = [name.split('_')[:-1] for name in names] 
+  df = pd.DataFrame(img_params, columns= keys)
+  filenames = names
+  imgs = []
+  for name in filenames:
+    image = Image.open('.\\resources\Car_img\\' + name)
+    imgs.append(transforms.ToTensor()(image))
+    image.close()
+  
+  for col_name in keys:
+    if df[col_name].dtype == 'object':
+      col_vals = list(df[col_name].unique())
+      n = len(col_vals)
+      vals = list(range(1, n+1))
+      di = dict(zip(col_vals, vals))
+      df[col_name] = df[col_name].map(di).fillna(0)
+  df.dropna()
+  df.insert(0, 'Img', imgs)
+  data = ImgDataset(df)
+  return data
+
 if __name__ == "__main__":
-    cars = CarsData()
-    cars.get_data()
+    #cars = CarsData()
+    #cars.get_data()
+    cars_imgs = get_images()
+    for idx in range(3):
+      x, y = cars_imgs[idx]
+    cars_loader = get_dataloader(cars_imgs, 4)
+    for x, y in cars_loader:
+      print(x)
+      print(y)
+      break
+
